82ff1b794c9233611cf5220d02db36fbcfc11a95
rubaiyat12 <rubaiyatraha12@gmail.com>
2023-10-20 10:13:13 -0700

    design part

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div>
+      <div className='arrow'></div>
       <ul className='circle'>
         <li>
-          <div className='text' contentEditable="true">
+          <div className='text' contentEditable="true" spellCheck="false">
             Ashik
-
           </div>
 
         </li>
+        <li>
+          <div className='text' contentEditable="true" spellCheck="false">
+            Anik
+          </div>
+
+        </li>
+        <li>
+          <div className='text' contentEditable="true" spellCheck="false">
+            Dip
+          </div>
+
+        </li>
+        <li>
+          <div className='text' contentEditable="true" spellCheck="false">
+            Niti
+          </div>
+
+        </li>
+        <li>
+          <div className='text' contentEditable="true" spellCheck="false">
+            Manzil
+          </div>
+
+        </li>
+        
 
       </ul>
+      <button className='button'>Spin It!</button>
       
     </div>
   );
