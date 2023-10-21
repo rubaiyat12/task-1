7c5d4c85038a8aa4a240994d27773bee9e6466aa
rubaiyat12 <rubaiyatraha12@gmail.com>
2023-10-20 22:13:43 -0700

    rotation part

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-
+import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  
+  state = {
+    name: "circle"
+
+  }
+
+ startRotation = () =>{
+    this.setState({
+      name:"circle start-rotate" 
+    });
+
+    setTimeout(() =>{
+      
+      this.setState({
+        name:"circle start-rotate stop-rotate",
+         
+        
+      });
+    }, Math.floor(Math.random()*5000)+1);
+    }
+
+
+  render(){
   return (
     <div>
       <div className='arrow'></div>
-      <ul className='circle'>
+      <ul className={this.state.name}>
         <li>
           <div className='text' contentEditable="true" spellCheck="false">
             Ashik
@@ -39,10 +62,12 @@ function App() {
         
 
       </ul>
-      <button className='button'>Spin It!</button>
+      <button className='button' onClick={this.startRotation}>Spin It!</button>
+      
       
     </div>
   );
+}
 }
 
 export default App;
